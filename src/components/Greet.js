ff6ff81d5c3046e5472fca4c385aa9c82e5c3c39
Rimsha-Urooj <rimshaurooj56@gmail.com>
2021-08-24 17:05:01 +0500

    functions file

---
+++ src/components/Greet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+// Normal Function
+// function Greet(){
+//     return <h1>Hey Rimsha</h1>
+// }
+
+// Arrow Function
+
+const Greet = () => <h1>Hey Rimsha Urooj</h1> 
+
+export default Greet
