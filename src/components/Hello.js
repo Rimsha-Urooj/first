89e10f360a28acbdd9ee9771eb6d501f78c741e1
Rimsha-Urooj <rimshaurooj56@gmail.com>
2021-08-25 12:42:07 +0500

    key value pair in createElement method

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,17 +2,21 @@ import React from 'react'
 
 function Hello (){
     // codse using JSX
-    return (
-        <div>
-            <h1>From Lahore</h1>
-            <h2>Graduated</h2>
-        </div>
-    )
+    // return (
+    //     <div>
+    //         <h1>From Lahore</h1>
+    //         <h2>Graduated</h2>
+    //     </div>
+    // )
 
     // Without using JSX
     // React library provides createElement method.
 
-    // return React.createElement('div', null, 'Hey User.');
+    return React.createElement(
+        'div',
+        {id:'first', style:{color:'blue'}}, 
+        React.createElement('h1',null,'Hello User')
+    );
 
 
 }
