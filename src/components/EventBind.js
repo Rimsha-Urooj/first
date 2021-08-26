76915f9690ffb2f125e395825c67218684bdb4d0
Rimsha-Urooj <rimshaurooj56@gmail.com>
2021-08-26 14:43:57 +0500

    4th approch for binding event handler

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -8,13 +8,23 @@ class EventBind extends Component {
              message: 'Hello',
         }
 
-        // 3rd Approch :- Bind the event handler in the constructor
+        // 3rd Approch :- Bind the event handler in the class constructor 
         this.clickHandler = this.clickHandler.bind(this)
     }
     
-    clickHandler(){
+
+    // For 1 - 3 Approches
+    // clickHandler(){
+    //     this.setState({
+    //         message: 'Good Bye',
+    //     })
+    // }
+
+
+    // Arrow function as a class property method for 4th approch
+    clickHandler = () => {
         this.setState({
-            message: 'Good Bye',
+            message: 'Goog Bye',
         })
     }
 
@@ -23,16 +33,20 @@ class EventBind extends Component {
             <div>
                 <div>{this.state.message}</div>
 
-                {/* First Approach */}
+                {/* First Approach :- Binding in render function */}
                 {/* Use of 'bind' keyowrd to bind the event handler */}
                 {/* <button onClick={this.clickHandler.bind(this)}>Click (Event Binding)</button> */}
 
-                {/* 2nd Approch */}
+                {/* 2nd Approch :- Arrow function in render function*/}
                 {/* Use Arrow Function in the {} and add () as we are calling a function and returning its value */}
                 {/* <button onClick={() => this.clickHandler()}>Click (Event Binding)</button> */}
 
-                {/* 3rd Approach */}
+                {/* 3rd Approach :- Binding in the class construtor */}
                 {/* Call the function without () in event handler */}
+                {/* <button onClick={this.clickHandler}>Click (Event Binding)</button> */}
+
+                {/* 4th Approach :- Class Property as an Arrow Function */}
+                {/* Change the way you define your method in the class */}
                 <button onClick={this.clickHandler}>Click (Event Binding)</button>
             </div>
         )
