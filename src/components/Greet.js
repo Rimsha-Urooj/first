c3daa65eac6ac9968368dac5c1cdbabb172091e9
Rimsha-Urooj <rimshaurooj56@gmail.com>
2021-08-26 10:58:32 +0500

    Destructuring props in functional component

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -8,14 +8,23 @@ import React from 'react'
 
 // Arrow Function
 
-const Greet = (props) => {
+// Destructing props in function parameter
+const Greet = ({name,heroName}) => {
     return (
         <div>
-            <h1>Greetings {props.name} your hero is {props.heroName}</h1>
-            {props.children}
+            <h1>Greetings {name} your hero is {heroName}</h1>
+            {/* {props.children} */}
         </div>
          
     )
 }
+
+// Two way to destructure.. 
+// 1. is applied above which is destructure it in the function parameter
+// => const Greet =(props) => {} transformed into => const Greet=({name, heroName})=> {}
+// 2. is to destructure in function body
+// => const Greet = (props) =>{
+//      const name, heroName = props;
+// }
 
 export default Greet
