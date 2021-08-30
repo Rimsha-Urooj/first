9bdd17f73a0ac88f77b23e1bd636766e668a802e
Rimsha-Urooj <rimshaurooj56@gmail.com>
2021-08-30 11:55:42 +0500

    index as keys

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,35 +1,38 @@
 import React from 'react'
-import Person from './Person'
+// import Person from './Person'
 
 function NameList() {
-    // const names = ['Bruce', 'Clark', 'Diana']
-    // const nameList = names.map(name => <h2>{name}</h2>)
-    const persons = [
-        {
-            id: 1,
-            name: 'Bruce',
-            age: 30,
-            skills: 'React'
-        },
-        {
-            id: 2,
-            name: 'Liana',
-            age: 33,
-            skills: 'Machine Learning'
-        },
-        {
-            id: 3,
-            name: 'David',
-            age: 23,
-            skills: 'Python'
-        }
-    ]
+    const names = ['Bruce', 'Clark', 'Diana', 'Bruce']
+    // Keys in Array
+    // Iterating with indexs
+
+    const nameList = names.map((name, index) => <h2 key={index}>{index} {name}</h2>)
+    // const persons = [
+    //     {
+    //         id: 1,
+    //         name: 'Bruce',
+    //         age: 30,
+    //         skills: 'React'
+    //     },
+    //     {
+    //         id: 2,
+    //         name: 'Liana',
+    //         age: 33,
+    //         skills: 'Machine Learning'
+    //     },
+    //     {
+    //         id: 3,
+    //         name: 'David',
+    //         age: 23,
+    //         skills: 'Python'
+    //     }
+    // ]
     // Simple Approach
     // const personList = persons.map(person => <h2>I am {person.name}. I am {person.age} years old. I am having skills in {person.skills}</h2>)
     
     // Refactoring person
     // Adding key props
-    const personList = persons.map(person=> <Person key={person.skills} person={person}/>)
+    // const personList = persons.map(person=> <Person key={person.skills} person={person}/>)
     return (
         <div>
             {/* Simple Method */}
@@ -40,7 +43,7 @@ function NameList() {
             {/* Using map Approach i.e, List rendering approach */}
            
            {
-               personList
+               nameList
           }
         </div>
     )
