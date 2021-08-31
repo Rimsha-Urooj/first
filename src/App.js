c20ad717d3555cfcd65723b3f950dde4af143a2d
Rimsha-Urooj <rimshaurooj56@gmail.com>
2021-08-31 17:33:42 +0500

    portal

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
 // import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreetingComponent from './components/UserGreetingComponent';
-import NameList from './components/NameList';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
+// import Greet from './components/Greet';
+// import Welcome from './components/Welcome';
+// import Hello from './components/Hello';
+// import Message from './components/Message';
+// import Counter from './components/Counter';
+// import FunctionClick from './components/FunctionClick';
+// import ClassClick from './components/ClassClick';
+// import EventBind from './components/EventBind';
+// import ParentComponent from './components/ParentComponent';
+// import UserGreetingComponent from './components/UserGreetingComponent';
+// import NameList from './components/NameList';
+// import Stylesheet from './components/Stylesheet';
+// import Inline from './components/Inline';
 import './appStyles.css'
+import PortalDemo from './components/PortalDemo';
 // Importing modules.css file
-import styles from './appStyles.module.css'
-import Form from './components/Form';
-import LifeCycleA from './components/LifeCycleA';
-import FragmentDemo from './components/FragmentDemo';
-import Table from './components/Table';
-import ParentComp from './components/ParentComp';
-import RefsDemo from './components/RefsDemo';
-import FocusInput from './components/FocusInput';
-import FRParentInput from './components/FRParentInput';
+// import styles from './appStyles.module.css'
+// import Form from './components/Form';
+// import LifeCycleA from './components/LifeCycleA';
+// import FragmentDemo from './components/FragmentDemo';
+// import Table from './components/Table';
+// import ParentComp from './components/ParentComp';
+// import RefsDemo from './components/RefsDemo';
+// import FocusInput from './components/FocusInput';
+// import FRParentInput from './components/FRParentInput';
+
 
 function App() {
   return (
     <div className="App">
 
+      {/* Portal Demo */}
+      <PortalDemo />
+
       {/* Forwarding Refs */}
-      <FRParentInput />
+      {/* <FRParentInput /> */}
 
       {/* Refs with class copmonents */}
       {/* <FocusInput /> */}
