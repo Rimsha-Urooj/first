b877863494848515699ecb1ea29db9d132f963ef
Rimsha-Urooj <rimshaurooj56@gmail.com>
2021-09-01 11:50:08 +0500

    HOC part 2, implementing and basic structure

--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -1,31 +1,21 @@
 import React, { Component } from 'react'
+import withCounter from './withCounter'
+
+
+// implementing HOC 
 
 class ClickCounter extends Component {
 
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            count:0
-        }
-    }
-
-    incrementCount = () => {
-        this.setState(prevState => {
-            return {
-                count: prevState.count+1
-            }
-        })
-    }
+   
     
     render() {
-        const { count } = this.state
+        const { count, incrementCount } = this.props
         return (
             <div>
-                <button onClick={this.incrementCount}>Clicked {count} times</button>
+                <button onClick={incrementCount}> Clicked {count} times</button>
             </div>
         )
     }
 }
 
-export default ClickCounter
+export default withCounter(ClickCounter)
--- src/components/HoverCounter.js
+++ src/components/HoverCounter.js
@@ -1,31 +1,20 @@
 import React, { Component } from 'react'
+import withCounter from './withCounter'
+
+
+// implementing HOC 
 
 class HoverCounter extends Component {
 
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            count:0
-        }
-    }
-
-    incrementCount = () => {
-        this.setState(prevState => {
-            return {
-                count: prevState.count+1
-            }
-        })
-    }
 
     render() {
-        const {count} = this.state
+        const {count, incrementCount} = this.props
         return (
             <div>
-                <h2 onMouseOver={this.incrementCount}>Hovered {count} times</h2>
+                <h2 onMouseOver={incrementCount}>Hovered {count} times</h2>
             </div>
         )
     }
 }
 
-export default HoverCounter
+export default withCounter(HoverCounter)
